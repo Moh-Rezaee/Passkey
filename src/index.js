e038edfef607cfa208dee904b5f80d2abef22b79
Moh Rezaee <MRezaee+aglemu@agl.com.au>
2025-04-03 14:55:46 +1100

    Updated

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//ReactDOM.render(<App />, document.getElementById('mohpage'));
+document.addEventListener('DOMContentLoaded', () => {
+    const rootElement = document.getElementById('root');
+    ReactDOM.render(<App />, rootElement);
+  });
